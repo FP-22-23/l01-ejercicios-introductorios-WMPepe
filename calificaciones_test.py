6b9c363cfe347ba0764b9605a2f305dbaf713702
Pepe Coronil <88033533+WMPepe@users.noreply.github.com>
2022-09-27 13:19:36 +0200

    Update calificaciones_test.py

--- calificaciones_test.py
+++ calificaciones_test.py
@@ -3,7 +3,7 @@ import calificaciones
 
 a = calificaciones.SolicitudAET()
 print(calificaciones.CalcularNota(a[0],a[1],a[2]))
-print(calificaciones.NotaContinua(calificaciones.SolicitarCuestionario(),calificaciones.SolicitarParciales(),calificaciones.SolicitarProyectos()))
+print(calificaciones.calcula_nota_evaluacion_continua(calificaciones.SolicitarCuestionario(),calificaciones.SolicitarParciales(),calificaciones.SolicitarProyectos()))
 
 
 
